3c7966bef82d8ea41a2a2090bedda2079bdd5478
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-08 17:21:17 +0530

    Buy and Sell Stocks Problem for decreasing stocks

---
+++ Arrays (Part 2)/11BuynSellStocksfordecstocks.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void maxProfit (int *prices, int n) {
+    int bestBuy[100000];
+    bestBuy[0] = INT_MAX;
+    cout << bestBuy[0] << ",";
+    for (int i=1; i<n; i++) {
+        bestBuy[i] = min(bestBuy[i-1], prices[i-1]);
+        cout << bestBuy[i] << ",";
+    }
+    cout << endl;
+
+    int maxProfit = 0;
+    for (int i=0; i<n; i++) {
+        int currProfit = prices[i] - bestBuy[i];
+        maxProfit = max(maxProfit, currProfit);
+    }
+
+    cout << "max Profit: " << maxProfit << endl;
+}
+
+int main() {
+    int prices[6] = {7, 6, 5, 4, 3, 2};
+    int n = sizeof(prices) / sizeof(int);
+    maxProfit(prices, n);
+    return 0;
+}
