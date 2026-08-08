6fef1d27fb2ada8d413dc9b7f53a3d416032677b
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-08 13:07:42 +0530

    Maximum Subarray Sum Slightly Optimized

---
+++ Arrays (Part 2)/6maxSubarraysumSlightlyOptimized.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void maxSubarraySum2 (int *arr, int n) {
+    int maxSum = INT_MIN;
+    for (int start=0; start < n; start++) {
+        int currSum = 0;
+        for (int end=start; end < n; end++) {
+            currSum += arr[end];
+            maxSum = max(maxSum, currSum);
+        }
+    }
+    cout << "maximum subarray sum: " << maxSum << endl;
+}
+
+int main() {
+    int arr[6] = {2, -3, 6, -5, 4, 2};
+    int n = sizeof(arr) / sizeof(int);
+
+    maxSubarraySum2(arr, n);
+    return 0;
+}
