630353edca14ce42e5f5284a15019f53ea3c3f51
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-08 10:29:36 +0530

    Print the starting and ending index of all the subarrays of an array

---
+++ Arrays (Part 2)/1PrintSubarraystartnendpoints.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printSubarray(int *arr, int n) {
+    for (int start=0; start<n; start++) {
+        for (int end = start; end < n; end++) {
+            cout << "(" << start << "," << end << ") ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+    printSubarray(arr, n);
+    return 0;
+}
