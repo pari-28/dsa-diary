d2a1e54d4be465378d09e3430040c84e092ffbd8
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-08 15:05:54 +0530

    Kadane's Algorithm for max subarray sum

---
+++ Arrays (Part 2)/7KadanesAlgorithmformaxsubarraysum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void maxSubarraySum3(int *arr, int n) {
+    int maxSum = INT_MIN;
+    int currSum = 0;
+    for (int i=0; i<n; i++) {
+        currSum += arr[i];
+        maxSum = max(maxSum, currSum);
+        if (currSum < 0) {
+        currSum = 0;
+        }
+    }
+    cout << "maximum subarray sum: " << maxSum << endl;
+}
+
+int main() {
+    int arr[] = {2, -3, 6, -5, 4, 2};
+    int n = sizeof(arr) / sizeof(int);
+
+    maxSubarraySum3(arr, n);
+    return 0;
+}
