f20d03b6962e47f4000b08a6e20cae799fd3c94a
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-10 17:02:57 +0530

    Trapping Water for descending bars/heights

---
+++ Arrays (Part 2)/17TrappingWater.cpp
@@ -0,0 +1,43 @@
+// using 0 as boundary condition
+#include <iostream>
+using namespace std;
+
+void trap(int *heights, int n) {
+    int leftMax[20000];
+    int rightMax[20000];
+
+    leftMax[0] = 0;
+    rightMax[0] = 0;
+
+    for (int i=1; i<n; i++) {
+        leftMax[i] = max(leftMax[i-1], heights[i-1]);
+    }
+
+    for (int i=n-2; i>=0; i--) {
+        rightMax[i] = max(rightMax[i+1], heights[i+1]);
+    }
+
+    int waterTrapped = 0;
+    for (int i=0; i<n; i++) {
+        int currTrapped = (min (leftMax[i], rightMax[i])) - heights[i];
+
+        if(currTrapped > 0) {
+            waterTrapped += currTrapped;
+            cout << currTrapped << ",";
+        }
+        else {
+            cout << 0 << ",";
+        }
+    }
+    cout << endl;
+    cout << "water trapped : " << waterTrapped << endl;
+
+}
+
+int main() {
+    int heights[] = {5, 4, 3, 2, 1};
+    int n = sizeof(heights) / sizeof(int);
+
+    trap(heights, n);
+    return 0;
+}
