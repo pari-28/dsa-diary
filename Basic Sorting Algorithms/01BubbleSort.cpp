77de3b368d4d81c7a3e3d74f2f75fbf185b0cf59
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-12 19:06:43 +0530

    Bubble sort in ascending order

---
+++ Basic Sorting Algorithms/01BubbleSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void printArr (int *arr, int n) {
+    for (int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+void bubbleSort (int *arr, int n) {
+    for (int i=0; i<n-1; i++) {
+        for (int j=0; j<n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap (arr[j], arr[j+1]);
+            }
+        }
+    }
+    printArr(arr, n);
+}
+
+int main() {
+    int arr[5] = {5, 4, 1, 3, 2};
+    bubbleSort(arr, 5);
+    return 0;
+}
