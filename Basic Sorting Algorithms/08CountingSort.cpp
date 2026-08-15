43a590e76c6c01e50717b276fbed20a672e57f8e
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-15 12:53:53 +0530

    Counting sort algorithm

---
+++ Basic Sorting Algorithms/08CountingSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void printArr (int *arr, int n) {
+    for (int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+void countSort (int *arr, int n) {
+    int freq[100000];
+    int minVal = INT_MAX, maxVal = INT_MIN;
+    for (int i=0; i<n; i++) {
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    //1st step
+    for (int i=0; i<n; i++) {
+        freq[arr[i]]++;
+    }
+
+    //2nd step
+    for (int i = minVal, j = 0; i < maxVal; i++) {
+        while (freq[i] > 0) {
+            arr[j++] = i;
+            freq[i]--;
+        }
+    }
+
+    printArr(arr, n);
+}
+
+int main() {
+    int arr[] = {1, 4, 1, 3, 2, 4, 3, 7};
+    countSort(arr, 8);
+    return 0;
+}
