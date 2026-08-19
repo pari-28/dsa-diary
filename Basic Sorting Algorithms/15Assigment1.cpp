08e43f41b204a00a55b00b35b048cb7b3359a9d2
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-19 15:14:21 +0530

    Assignment 1 Bubble sort in desending order

---
+++ Basic Sorting Algorithms/15Assigment1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void print(int *arr, int n) {
+    for (int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort (int *arr, int n) {
+    for (int i=0; i<n-1;  i++) {
+        bool isSwap = false;
+        for (int j=0; j<n-i-1; j++) {
+            if (arr[j] < arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                isSwap = true;
+            }
+        }
+        if (!isSwap) {
+            break;
+        }
+    }
+
+    print(arr, n);
+}
+
+int main() {
+    int arr[10] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+    bubbleSort(arr, 10);
+
+    return 0;
+}
