69f605946e028c95b5a87752afdfa6c80a8a8723
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-22 16:49:45 +0530

    Introduction to 2d Array

---
+++ 2D Arrays/01Arrays_2d.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int students[3][3] = { {100, 100, 100},
+                        {85, 74, 89},
+                        {63, 72, 65}};
+    cout << students[1][1] << endl;
+    return 0;
+}
