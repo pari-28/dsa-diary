10ad8e29d10e545c048aaccef89ffaaadf7d0b1b
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-26 19:41:44 +0530

    Search in sorted matrix brute force approach by using bool function

---
+++ 2D Arrays/09SearchinSortedArray.cpp
@@ -0,0 +1,25 @@
+//Brute force 
+#include <iostream>
+using namespace std;
+
+bool search(int matrix[][4], int n, int m, int key) {
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            if (matrix[i][j] == key) {
+                cout << "key found at : " << i << "," << j << endl;
+                return true;
+            }
+        }
+    }
+    cout << "key not found" << endl;
+    return false;
+}
+
+int main() {
+    int matrix[4][4] = {{10, 20, 30, 40},
+                        {15, 25, 35, 45},
+                        {27, 29, 37, 48},
+                        {32, 33, 39, 50}};
+    search(matrix, 4, 4, 33);
+    return 0;
+}
