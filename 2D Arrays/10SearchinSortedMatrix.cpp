f2b871c5c825eac5ec11a5f4f90cea062e625500
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-26 20:00:40 +0530

    Search in sorted matrix by row wise binary search

---
+++ 2D Arrays/10SearchinSortedMatrix.cpp
@@ -0,0 +1,35 @@
+//row-wise/column-wise Binary Search
+
+//rowwise 
+
+#include <iostream>
+using namespace std;
+
+void search(int matrix[][4], int n, int m, int key) {
+    for (int i=0; i<n; i++) {
+        int start=0, end=m-1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if (matrix[i][mid] == key) {
+                cout << "key found at : (" << i << "," << mid << ")" << endl;
+                return;
+            }
+            else if (matrix[i][mid] > key) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+    }
+    cout << "key not found." << endl;
+}
+
+int main() {
+    int matrix[4][4] = {{10, 20, 30, 40},
+                        {15, 25, 35, 45},
+                        {27, 29, 37, 48},
+                        {32, 33, 39, 50}};
+    search(matrix, 4, 4, 20);
+    return 0;
+}
