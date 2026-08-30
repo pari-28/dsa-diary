7e1b5c006f2bd6bcb85f07110c8ad5093ae892cc
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-30 16:50:30 +0530

    Basics of char array

---
+++ Char Arrays and Strings/01charArrayBasic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch1 = 'a';
+    char ch2 = 'A';
+    char ch3 = '9';
+    char ch4 = '%';
+    char ch5 = '\n'; //next line char -> \n
+    char ch6 = '\t'; //tab space
+    char ch7 = 'X';
+
+    return 0;
+}
