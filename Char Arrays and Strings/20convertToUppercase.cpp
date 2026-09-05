65dbbcbc0a16696a2a7a5f774e7f80dd58a04b74
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-05 16:38:07 +0530

    convert to upper case

---
+++ Char Arrays and Strings/20convertToUppercase.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void toUpper( char word[], int n) {
+    for (int i=0; i<n; i++) {
+       char ch = word[i];
+       if (ch >= 'A' && ch <= 'Z') {
+        continue;
+       } else {
+        word[i] = ch - 'a' + 'A';
+       }
+    }
+}
+
+int main() {
+    char word[] = "ApPle";
+    toUpper(word, strlen(word));
+    cout << word << endl;
+}
