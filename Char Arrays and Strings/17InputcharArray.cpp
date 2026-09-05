1a8adf064c36ef62c6176937bd923794f2ac4827
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-05 14:37:33 +0530

    Input char array by cin

---
+++ Char Arrays and Strings/17InputcharArray.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char word[30];
+    cin >> word;
+
+    cout << "your word was : " << word << endl;
+    cout << "length : " << strlen(word) << endl;
+    return 0;
+}
