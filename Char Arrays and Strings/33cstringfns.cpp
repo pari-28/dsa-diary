2b0f39214323125921307d528f71e85fc112d39b
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-09 16:52:38 +0530

    string compare function (1st letter is compared)

---
+++ Char Arrays and Strings/33cstringfns.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str1[100] = "mango";
+    char str2[100] = "strawberry";
+    cout << strcmp(str1, str2) << endl;
+    return 0;
+}
