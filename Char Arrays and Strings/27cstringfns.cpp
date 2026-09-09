aaa7941c3b9241507ef147644403596f6c9d64f1
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-09 16:40:45 +0530

    Concatenate function

---
+++ Char Arrays and Strings/27cstringfns.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str1[100] = "abc";
+    char str2[100] = "xyz";
+    strcat(str1, str2);
+    cout << str1 << endl;
+    cout << str2 << endl; // no change
+    return 0;
+}
