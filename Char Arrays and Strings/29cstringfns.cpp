4884d3e9c828fa76a55e034af68feb71c30d937b
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-09 16:46:40 +0530

    string compare function (for equal string o/p is zero

---
+++ Char Arrays and Strings/29cstringfns.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str1[100] = "abc";
+    char str2[100] = "abc";
+    cout << strcmp(str1, str2) << endl;
+    return 0;
+}
