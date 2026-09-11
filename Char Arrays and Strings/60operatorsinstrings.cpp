53edf169e60800edf57c97b2003d45ac7d369e0b
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-11 10:50:50 +0530

    greaterthan comparison operator in string

---
+++ Char Arrays and Strings/60operatorsinstrings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1 = "cat";
+    string str2 = "dog";
+
+    cout << (str1 > str2) << endl; //false -> 0
+    return 0;
+}
