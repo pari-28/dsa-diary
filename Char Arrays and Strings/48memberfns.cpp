98452004b83ca8ee1196bd1851d160d44d948d8e
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-11 07:39:46 +0530

    find function to find the word in the string

---
+++ Char Arrays and Strings/48memberfns.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "I love coding in c++ & java. I don't like python";
+    cout << str.find("c++") << endl;
+    return 0;
+}
