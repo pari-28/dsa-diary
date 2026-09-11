74034dfd2ce0896e201d1e0d8d40df337f786d63
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-11 10:47:39 +0530

    notequalto comparison operator in string

---
+++ Char Arrays and Strings/58operatorsinstrings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1 = "cat";
+    string str2 = "cat";
+
+    cout << (str1 != str2) << endl;
+    return 0;
+}
