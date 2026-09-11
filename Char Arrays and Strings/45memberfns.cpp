db0a0e2556ca0c0f0f9c235db68ea761f57f68fb
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-11 07:20:51 +0530

    value at any index using member function

---
+++ Char Arrays and Strings/45memberfns.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "apna college!";
+    cout << str.length() << endl;
+    cout << str[3] << endl;
+    cout << str.at(3) << endl;
+    return 0;
+}
