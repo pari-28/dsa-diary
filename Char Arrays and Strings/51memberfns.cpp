9a8986843d3bdd2dd413c37d0728b184dc7b1842
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-11 07:50:15 +0530

    find the 3rd occurance of the word

---
+++ Char Arrays and Strings/51memberfns.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "I love coding in c++ & c++. I don't like c++";
+    
+    cout << str.find("c++", 25) << endl;
+    return 0;
+}
