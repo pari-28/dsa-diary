9c38b10c66a8fd00575781dcd698b691d7587dda
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-09-11 16:19:55 +0530

    number of lowercase vowels in the string

---
+++ Char Arrays and Strings/61AssignmentQue1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int getVowelCount(string str) {
+    int vowCount = 0;
+
+    for (int i=0; i<str.length(); i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') {
+            vowCount++;
+        }
+    }
+    cout << vowCount << endl;
+    return vowCount;
+}
+
+int main() {
+    string str;
+    getline(cin, str);
+    getVowelCount(str);
+    return 0;
+}
